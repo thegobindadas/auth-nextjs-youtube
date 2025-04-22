b6af2b55ca44c6c03aabace4458e8083cd59fc18
Gobinda Das <dasgobinda9955@gmail.com>
2025-04-22 16:13:12 +0530

    add signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+
+
+
+export default function SignupPage() {
+
+    const [user, setUser] = React.useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+            <label htmlFor="username">Username</label>
+            <input
+                className="px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+                className="px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200"
+                id="email"
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="abc@xyz.com"
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                className="px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+
+            <button 
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            >
+                Signup
+            </button>
+
+            <p className="text-gray-600">
+                Already have an account?{' '}
+                <a href="/login" className="text-indigo-600 hover:text-indigo-800 font-medium transition duration-200">
+                    Log in here
+                </a>
+          </p>
+        
+        </div>
+    )
+}
