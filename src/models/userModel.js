df8cfebfaf369a5fca65d7f8bad865b245f63ec5
Gobinda Das <dasgobinda9955@gmail.com>
2025-04-22 16:41:36 +0530

    add user model

---
+++ src/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date
+})
+
+
+
+const User = mongoose.models.users || mongoose.model('users', userSchema);
+
+export default User;
